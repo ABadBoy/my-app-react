978f41d90bc599f3b20198b56f875a18ff33739b
abadboy <peter.zhao@enisco.com>
2022-06-02 14:45:25 +0800

    add props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import Greet from "./components/Greet";
+import Welcome from "./components/Welcome";
+import Hello from "./components/hello";
 
 class App extends React.Component{
   render() {
     return (
         <div className="App">
           <p>Hello World!</p>
-          <Greet/>
+          <Greet name="Angel"/>
+          <Greet name="Mike"/>
+          <Greet name="Jerry"/>
+          <Welcome name="Nick"/>
+          <Welcome name="Jim"/>
+          <Welcome name="Jeff"/>
         </div>
     )
   }
